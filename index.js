37dad63cf13b9e5d111636cd19b9a602d0e8fabb
corina <corinabe93@gmail.com>
2020-07-11 18:19:54 +0200

    books added to DB

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
 const corsMiddleWare = require("cors");
 const { PORT } = require("./config/constants");
 const jsonParser = express.json();
 
-const booksRouter = require('./routers/books');
-const categoryRouter = require('./routers/category');
-const orderRouter = require('./routers/order');
-const paymentRouter = require('./routers/payments')
-const userRouter = require('./routers/users');
+const booksRouter = require("./routers/books");
+const categoryRouter = require("./routers/category");
+const orderRouter = require("./routers/order");
+const paymentRouter = require("./routers/payments");
+const userRouter = require("./routers/users");
 
 
 app.use(corsMiddleWare());
@@ -21,10 +21,11 @@ if (process.env.DELAY) {
   });
 }
 
-app.use('/books', booksRouter);
-app.use('/categories', categoryRouter);
-app.use('/orders', orderRouter);
-app.use('/checkout', paymentRouter);
-app.use('/users', userRouter);
+app.use("/books", booksRouter);
+app.use("/categories", categoryRouter);
+app.use("/orders", orderRouter);
+app.use("/checkout", paymentRouter);
+app.use("/users", userRouter);
 
-app.listen(PORT, () => console.log(`Listening to port ${PORT}`))
+
+app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
